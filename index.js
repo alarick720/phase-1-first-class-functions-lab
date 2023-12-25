b478f31278ae53413489cd933d922986c4d7a785
Alarick Whitted <awhitted834@gmail.com>
2023-12-25 17:45:23 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+ return   ['Antonia', 'Nuru'];
+}
+function returnLastTwoDrivers(drivers){
+    return ['Amari', 'Mo'];
+
+}
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice['Antonia','Nuru'];
+  }
+
+const selectingDrivers = [returnFirstTwoDrivers];
+function returnFirstTwoDrivers(drivers) {
+    return ['Antonia', 'Nuru'];
+    }
+
+     function returnLastTwoDrivers(drivers){
+            return ['Amari', 'Mo'];
+        }
+        selectingDrivers.push(returnLastTwoDrivers);
+        function returnLastTwoDrivers(drivers){
+            return ['Amari', 'Mo'];
+        }
+        
+        function createFareMultiplier(fareMultiplier) {
+            return function(num) {
+              return num * fareMultiplier;
+            };
+           
+        }
+        const fareDoubler = createFareMultiplier(2);
+        const fareTripler = createFareMultiplier(3);
+
+        function selectDifferentDrivers(arrayOfDrivers, returnFunction) {
+            return returnFunction(arrayOfDrivers);
+          }
+          
+          function returnLastTwoDrivers(drivers) {
+            return drivers.slice(-2);
+          }
+          const drivers = ['Amari', 'Mo', 'Antonia', 'Nuru'];
+          const result = selectDifferentDrivers(drivers, returnLastTwoDrivers);
+            
+            
+        
+          
+        
+        
+
+        
+        
+
+        
+       
+
+        
+        
+           
+                  
+                
+            
+        
+            
+                
+                
+        
+            
+          
+          
+        
+            
+        
+          
+                 
+                  
+        
+        
+        
+      
+  
+  
+   
+
